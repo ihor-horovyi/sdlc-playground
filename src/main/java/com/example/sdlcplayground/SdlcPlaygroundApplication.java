127ad242b97389a1d8460e2c9e820cdad4b45c19
ihorhorovyi <ihor.horovyi@vitechteam.com>
2023-08-20 22:03:51 +0200

    added checkstyle

--- src/main/java/com/example/sdlcplayground/SdlcPlaygroundApplication.java
+++ src/main/java/com/example/sdlcplayground/SdlcPlaygroundApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SdlcPlaygroundApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SdlcPlaygroundApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SdlcPlaygroundApplication.class, args);
+    }
 
 }
