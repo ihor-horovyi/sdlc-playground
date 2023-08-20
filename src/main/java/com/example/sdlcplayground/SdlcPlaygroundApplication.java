715807e0d1d48cb7df9373813efbd36dca194141
ihorhorovyi <ihor.horovyi@vitechteam.com>
2023-08-20 21:44:16 +0200

    project init

---
+++ src/main/java/com/example/sdlcplayground/SdlcPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.example.sdlcplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdlcPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdlcPlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sdlcplayground/SdlcPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sdlcplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdlcPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
